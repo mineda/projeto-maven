73589074e18330c868bf0fc2a6aa9f221ab97b41
Lucas-Povinske <lucasjp96@gmail.com>
2020-09-01 23:23:30 +0000

    Update on Tests

--- src/test/java/br/gov/sp/fatec/projetomaven/AppTest.java
+++ src/test/java/br/gov/sp/fatec/projetomaven/AppTest.java
@@ -16,12 +16,26 @@ public class AppTest
      * Rigourous Test :-)
      */
     @Test
-    public void testSoma()
-    {
+    public void testSoma(){
         final Calculadora calc = new Calculadora();
         assertEquals(calc.soma(), 10.0, DELTA);
+    }
+
+    @Test
+    public void testSub(){
+        final Calculadora calc = new Calculadora();
         assertEquals(calc.sub(), 0.0, DELTA);
+    }
+
+    @Test
+    public void testMult(){
+        final Calculadora calc = new Calculadora();
         assertEquals(calc.mult(), 25.0, DELTA);
+    }
+
+    @Test
+    public void testDiv(){
+        final Calculadora calc = new Calculadora();
         assertEquals(calc.div(), 1.0, DELTA);
     }
 }
